9ec146a9d2b43d355055f2767cee81dc25857970
Lofty | Brambles <prantikdas2004@gmail.com>
2022-08-09 16:17:16 +0530

    🛣 chore: Adds base routes

---
+++ src/Routing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Routing = () => (
+	<div className="App">
+		<BrowserRouter>
+			<Routes>
+				<Route />
+			</Routes>
+		</BrowserRouter>
+	</div>
+);
+
+export default Routing;
